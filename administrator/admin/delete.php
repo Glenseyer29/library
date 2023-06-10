34d6aaae920d2aad7b99706f0f422c009367ba5d
R055 <rectoross@gmail.com>
2023-06-10 13:22:23 +0800

    delete

--- administrator/admin/delete.php
+++ administrator/admin/delete.php
@@ -1,19 +1,5 @@
 <?php
-<<<<<<< HEAD
 $connection = mysqli_connect("localhost", "root", "", "dbs");
-=======
-session_start(); //to display message
-$server = "localhost";
-$username = "root";
-$password = "";
-$dbname = "dbs";
-
-$conn = new mysqli($server, $username, $password, $dbname);
-
- if(isset($_POST['delete'])){
-   $STUDENTID =$_POST['ID'];
-   $STUDENTID =$_POST['STUDENTID'];
->>>>>>> ac654521561450feeec973def82a4179bed001fb
 
 if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
